c1ed9a32bd0ee60dee57c63a79dca9004d3d4be6
Elias Nadder <eliasn.learning@gmail.com>
2025-06-15 13:41:40 +0300

    removed

--- src/interfaces/IPropertyDetailsResponse.ts
+++
@@ -1,7 +0,0 @@
-
-import IProperty, { IPropertyAPI } from "./IProperty";
-
-export interface IPropertyDetailsResponse {
-  property: IPropertyAPI; // Raw API response
-  relaitedproperties: IPropertyAPI[]; // Raw API response
-}
