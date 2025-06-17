09e851053e4b920519f9fbdf099af827658710f6
Elias Nadder <eliasn.learning@gmail.com>
2025-06-17 11:48:44 +0300

    Refactor code

--- src/components/properties/PropertyList.tsx
+++ src/components/properties/PropertyList.tsx
@@ -5,14 +5,13 @@ import IProperty from "@/interfaces/IProperty";
 
 interface PropertyListProps {
   properties: IProperty[];
-  formatPrice: (price: number, currency: string) => string;
 }
 
-const PropertyList: React.FC<PropertyListProps> = ({ properties, formatPrice }) => {
+const PropertyList: React.FC<PropertyListProps> = ({ properties }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
       {properties.map((property) => (
-        <PropertyCard key={property.id} property={property} formatPrice={formatPrice} />
+        <PropertyCard key={property.id} property={property} />
       ))}
     </div>
   );
