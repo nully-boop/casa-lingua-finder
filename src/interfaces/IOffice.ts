e22f6653ccbebed0315bbcf48aad5e75a64eb0b7
Elias Nadder <eliasn.learning@gmail.com>
2025-07-21 10:41:24 +0300

    Add interface for office

---
+++ src/interfaces/IOffice.ts
@@ -0,0 +1,18 @@
+interface IOffice {
+  id: number;
+  name: string;
+  description?: string;
+  location?: string;
+  phone: string;
+  type: string;
+  status: string;
+  free_ads: number;
+  followers_count: number;
+  views: number;
+  image?: { url: string };
+  bio?: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export default IOffice;
